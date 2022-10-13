5eb5374ec82958e7bc35363fbc4847748724e819
minju0809 <loverminju88@naver.com>
2022-10-13 22:58:30 +0900

    내용 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,7 @@ const Card = () => {
         <div>즐겨찾기</div>
       </div>
       <div className="card-middle">
+        {/* switch문 사용해볼 예정 */}
         <div>상태</div>
         <h5>미세먼지 수치</h5>
         <h5>(현재 시간)</h5>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
+// 클릭 시 해당 컴포넌트만 보여주는 방식 (조건문)
+
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div>내 지역보기</div>
-      <div>전체 시도보기</div>
-      <div>즐겨찾기</div>
-    </div>
-  )
-}
+      <div className="footer-container">
+        <button className="myArea">내 지역보기</button>
+        <button className="all">전체 시도보기</button>
+        <button className="bopkmark">즐겨찾기</button>
+      </div>
+  );
+};
 
-export default Footer
+export default Footer;
